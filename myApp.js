8741ec34b9ac5e02bf59f1bac1ec8e9e386a06fa
valennegrelli02 <valentin.negrelli@mi.unc.edu.ar>
2023-01-17 19:18:21 -0300

    More CRUD ops in Person db

--- myApp.js
+++ myApp.js
@@ -83,19 +83,32 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, function (err, data) {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
 
-  done(null /*, data*/);
+  Person.remove({name: nameToRemove}, function (err, data) {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person.find({favoriteFoods: {$elemMatch: {$eq: foodToSearch}}})
+    .sort({name: 1})
+    .limit(2)
+    .select({age: 0})
+    .exec(function (err, data) {
+      if (err) return console.log(err);
+      done(null, data);
+    });
 };
 
 /** **Well Done !!**
